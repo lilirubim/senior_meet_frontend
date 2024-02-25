0d92d47c4640ec6ca2c00da3e4ed50602ac78499
lilirubim <148784760+lilirubim@users.noreply.github.com>
2024-02-24 21:03:32 -0800

    pantallas, navbar, footer

---
+++ senior_meet/senior-meet-frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'senior-meet-frontend';
+}
